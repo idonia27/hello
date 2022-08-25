34b2a52ac1b02d1f2469fe205413f3231341897c
idonia27 <111659728+idonia27@users.noreply.github.com>
2022-08-25 17:30:45 +0000

    initialcommit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Isabella Donia
+ * Date: 2022/23/8
+ *
+ * A simple hello world program in C
+ *
+ **/
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv){
+
+    printf("Isabella Donia\n");
+
+    return 0;
+ }
